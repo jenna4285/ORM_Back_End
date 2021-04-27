dd87b5538fd5aba565a28a0eaf9a768b97e0c10b
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-27 15:13:40 -0500

    category.js model complete

--- models/Category.js
+++ models/Category.js
@@ -6,23 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-    //     id
-
-    // Integer.
-
-    // Doesn't allow null values.
-
-    // Set as primary key.
-
-    // Uses auto increment.
-
-    // category_name
-
-    // String.
-
-    // Doesn't allow null values.
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   },
   {
     sequelize,
     timestamps: false,
