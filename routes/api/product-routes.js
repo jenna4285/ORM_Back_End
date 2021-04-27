66d73aff8c637059a538e010b2a9d69c89e43171
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-27 17:25:16 -0500

    product-routes: completed get all, and get one, and tested both...create method WIP

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,25 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({ include: [{model:Category}, {model:Tag}] }).then((completeProductData) => {
+    res.json(completeProductData);
+  }).catch((error) => {
+    res.json(error);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({where: {id: req.params.id }, include:[{model:Category}, {model:Tag}]})
+  .then((completeProductData) => {
+    res.json(completeProductData);
+  }) .catch((error) => {
+    res.json(error);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({ 
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: [req.body.tagIds]
+  }) 
+  // Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
