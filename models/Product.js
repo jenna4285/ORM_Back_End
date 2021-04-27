2ca36706d4c920f5ba11436f045c1ff6cf0671d6
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-27 15:28:55 -0500

    product.js model column complete

--- models/Product.js
+++ models/Product.js
@@ -9,46 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-    id
-
-// Integer.
-
-// Doesn't allow null values.
-
-// Set as primary key.
-
-// Uses auto increment.
-
-// product_name
-
-// String.
-
-// Doesn't allow null values.
-
-// price
-
-// Decimal.
-
-// Doesn't allow null values.
-
-// Validates that the value is a decimal.
-
-// stock
-
-// Integer.
-
-// Doesn't allow null values.
-
-// Set a default value of 10.
-
-// Validates that the value is numeric.
-
-// category_id
-
-// Integer.
-
-// References the Category model's id.
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+   },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      validate: {
+        isDecimal: true,
+      },
+    },
+     stock: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+     },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id',
+        },
+      },
   },
   {
     sequelize,
